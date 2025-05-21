646d5ab8e5618904e418a59a0152d420749db8ab
Joakim Hindersson <joakim.hindersson@elastx.se>
2025-05-21 20:19:33 +0200

    Add VMMode optimizations and disable WinRE initialization

--- scripts/win-common/sysprep.bat
+++ scripts/win-common/sysprep.bat
@@ -1,1 +1,1 @@
-%WINDIR%\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:A:\unattend.xml
+%WINDIR%\System32\Sysprep\sysprep.exe /generalize /oobe /mode:vm /shutdown /unattend:A:\unattend.xml
